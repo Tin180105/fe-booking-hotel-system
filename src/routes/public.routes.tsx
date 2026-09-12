fc8eb106c4c5494832e1d3477d3ad15a65aba659
Tin Dev <buiductin123tg@gmail.com>
2026-09-12 10:04:04 +0700

    fix page header

--- src/routes/public.routes.tsx
+++ src/routes/public.routes.tsx
@@ -11,6 +11,7 @@ import SearchResults from '../pages/SearchResults/SearchResults'
 import Rooms from '../pages/Rooms/Rooms'
 import Promotions from '../pages/Promotions'
 import Payment from '../pages/Payment'
+import Bookings from '../pages/Bookings/Bookings'
 
 import path from '../constants/path'
 
@@ -52,6 +53,11 @@ const publicRoutes: RouteObject[] = [
       {
         path: '/payment',
         element: <Payment />
+      },
+
+      {
+        path: path.bookings,
+        element: <Bookings />
       },
 
       {
